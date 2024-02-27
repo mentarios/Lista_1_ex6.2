094d9df7536dc5fe0809e513ea362171248e1184
mentarios <matmentarios@gmail.com>
2024-02-27 14:51:24 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            Console.WriteLine("digite um numero inteiro");
+            n = double.Parse(Console.ReadLine());
+            bool divisivel = false;
+            if (n / 4 == (int)n / 4)
+            {
+                divisivel = true;
+            }
+            if (divisivel)
+            {
+                Console.WriteLine("numero valido");
+            }
+            else Console.WriteLine("numero invalido");
+            Console.ReadKey();
+        }
+    }
+}
